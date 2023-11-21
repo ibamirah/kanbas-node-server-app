fc9e35c8455a14d1706e8a7bb9e65f17d6fcad48
ibamirah <amirahibrahim22@gmail.com>
2023-11-20 22:35:23 -0500

    first node commit ahhh

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import Hello from "./hello.js"
+import Lab5 from "./lab5.js"
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import "dotenv/config";
+
+// const express = require('express')
+const app = express()
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app)
+Lab5(app)
+app.use(cors());
+app.listen(process.env.PORT || 4000);
