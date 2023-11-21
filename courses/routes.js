8caf1b00dcc0c880cc1fcf1c35472f29101bfa98
ibamirah <amirahibrahim22@gmail.com>
2023-11-20 23:31:44 -0500

    trying to deploy

--- courses/routes.js
+++ courses/routes.js
@@ -1,35 +1,38 @@
 import Database from "../Database/index.js";
 function CourseRoutes(app) {
-    app.get("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        const course = Database.courses
-          .find((c) => c._id.$oid === id);
-        if (!course) {
-          res.status(404).send("Course not found");
-          return;
-        }
-        res.send(course);
-      });
-    
-    app.put("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        const course = req.body;
-        console.log(req.body);
-        Database.courses = Database.courses.map((c) =>
-          c._id === id ? { c, ...course } : c
-        );
-        res.sendStatus(204);
-      });  
-    app.delete("/api/courses/:id", (req, res) => {
-        const { id } = req.params;
-        Database.courses = Database.courses
-          .filter((c) => c._id !== id);
-        res.sendStatus(204);
-      });    
-     app.post("/api/courses", (req, res) => {
-        console.log(req.body);
-    const course = { ...req.body,
-      _id: new Date().getTime().toString() };
+  
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id.$oid === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = req.body;
+    console.log(req.body);
+    Database.courses = Database.courses.map((c) =>
+      c._id === id ? { c, ...course } : c
+    );
+    res.sendStatus(204);
+  });
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses
+      .filter((c) => c._id !== id);
+    res.sendStatus(204);
+  });
+  app.post("/api/courses", (req, res) => {
+    console.log(req.body);
+    const course = {
+      ...req.body,
+      _id: new Date().getTime().toString()
+    };
     Database.courses.push(course);
     console.log(Database.courses);
     res.send(course);
